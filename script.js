7f97b4214590c77a57be822b3ef6fd08387b39a4
Emmanuel <emmanuelola961@gmail.com>
2024-02-05 02:00:52 -0800

    Basic structure

---
+++ script.js
@@ -0,0 +1,22 @@
+const fetchMe = () => {
+    let url = 'https://musicapi-19wk.onrender.com/music/myAPI'
+    fetch(url)
+        .then(res => res.json())
+        .then((conRes) => {
+            console.log(conRes);
+            conRes.map((song) => {
+                musics.innerHTML += `
+                    <div class="musicIts">
+                        <img src="${song.songImage}" alt="" class="songImg"/> 
+                        <div>
+                            <p>${song.songTitle}</p>
+                            <p>${song.artistName}</p>
+                        </div>
+                    </div>
+                `
+            })
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+}
