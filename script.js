7a61428b52d7d1fb8f6c56f64b05a464a01fb76e
Emmanuel <emmanuelola961@gmail.com>
2024-02-06 09:24:39 -0800

    Louder added

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
-// const fetchMe = () => {
-//     let url = 'https://musicapi-19wk.onrender.com/music/myAPI'
-//     fetch(url)
-//         .then(res => res.json())
-//         .then((conRes) => {
-//             console.log(conRes);
-//             conRes.map((song) => {
-//                 musics.innerHTML += `
-//                     <div class="musicIts">
-//                         <img src="${song.songImage}" alt="" class="songImg"/> 
-//                         <div class="songTA">
-//                             <p>${song.songTitle}</p>
-//                             <p>${song.artistName}</p>
-//                         </div>
-//                         <div>
-//                             <div></div>
-//                             <button class="playButton" onclick="play(${song.artistName})">Play</button>
-//                         </div>
-//                     </div>
-//                 `
-//             })
-//         })
-//         .catch((err)=>{
-//             console.log(err);
-//         })
-// }
+setTimeout(() => {
+    // document.getElementById('loader').style.display = 'none'
+    document.getElementById('innerSection').style.height = '100%'
+}, 2000);
 
 const fetchMe = async () => {
     let url = 'https://musicapi-19wk.onrender.com/music/myAPI'
