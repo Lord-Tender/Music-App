5176a7ba9da1334dbc689e14494a2df15416696a
Emmanuel <emmanuelola961@gmail.com>
2024-02-06 05:27:54 -0800

    Play Button Added

--- script.js
+++ script.js
@@ -1,26 +1,59 @@
-const fetchMe = () => {
+// const fetchMe = () => {
+//     let url = 'https://musicapi-19wk.onrender.com/music/myAPI'
+//     fetch(url)
+//         .then(res => res.json())
+//         .then((conRes) => {
+//             console.log(conRes);
+//             conRes.map((song) => {
+//                 musics.innerHTML += `
+//                     <div class="musicIts">
+//                         <img src="${song.songImage}" alt="" class="songImg"/> 
+//                         <div class="songTA">
+//                             <p>${song.songTitle}</p>
+//                             <p>${song.artistName}</p>
+//                         </div>
+//                         <div>
+//                             <div></div>
+//                             <button class="playButton" onclick="play(${song.artistName})">Play</button>
+//                         </div>
+//                     </div>
+//                 `
+//             })
+//         })
+//         .catch((err)=>{
+//             console.log(err);
+//         })
+// }
+
+const fetchMe = async () => {
     let url = 'https://musicapi-19wk.onrender.com/music/myAPI'
-    fetch(url)
-        .then(res => res.json())
-        .then((conRes) => {
-            console.log(conRes);
-            conRes.map((song) => {
-                musics.innerHTML += `
+    let response = await fetch(url)
+    let songDetail = await response.json()
+    console.log(songDetail);
+    for (let index = 0; index < songDetail.length; index++) {
+        musics.innerHTML += `
                     <div class="musicIts">
-                        <img src="${song.songImage}" alt="" class="songImg"/> 
+                        <img src="${songDetail[index].songImage}" alt="" class="songImg"/> 
                         <div class="songTA">
-                            <p>${song.songTitle}</p>
-                            <p><i class="fas fa-user"></i>${song.artistName}</p>
+                            <p>${songDetail[index].songTitle}</p>
+                            <p>${songDetail[index].artistName}</p>
                         </div>
-                        <div>
-                            <button></button>
-                            <button></button>
+                        <div class="buttonArea">
+                            <div class=""><i class="fas fa-heart fovIcon" onclick="addFov()"></i></div>
+                            <button class="playButton" onclick="play(${index})">Play</button>
                         </div>
                     </div>
                 `
-            })
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
+
+    }
+}
+
+const play = async (indexNumber) => {
+    let url = 'https://musicapi-19wk.onrender.com/music/myAPI'
+    let get = await fetch(url)
+    let result = await get.json()
+    console.log(result[0]);
+    var music = result[indexNumber].songUrl
+    var song = new Audio(music)
+    song.play()
 }
